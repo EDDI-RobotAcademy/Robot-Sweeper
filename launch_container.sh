543e726495ae7c2ada6feaa9a3e02da042c18e1b
silenc3502 <gcccompil3r@gmail.com>
2024-04-03 01:10:38 +0900

    add examples

--- launch_container.sh
+++ launch_container.sh
@@ -14,6 +14,7 @@ docker run --gpus 0 --privileged --rm -it \
 		   -v /dev:/dev \
 		   -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
 		   -v $current_location/workspace:/home/melodic/workspace \
+		   -v $current_location/examples:/home/melodic/examples \
 		   -w /home/melodic/workspace \
            --shm-size=1gb \
            --env="XAUTHORITY=${XAUTH}" \
